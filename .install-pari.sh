c11b3c92ca9559fe4f6369ecbc413170a54fb781
Pascal <molin.maths@gmail.com>
2019-02-01 11:45:43 +0100

    upgrade pari version

--- .install-pari.sh
+++ .install-pari.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-if [ ! -f pari-2.9.4/Makefile ]; then
+VERSION="2.11.1"
+if [ ! -f pari-$VERSION/Makefile ]; then
     echo "downloading pari"
-    wget https://pari.math.u-bordeaux.fr/pub/pari/unix/pari-2.9.4.tar.gz
-    tar -xf pari-2.9.4.tar.gz
+    wget https://pari.math.u-bordeaux.fr/pub/pari/unix/pari-$VERSION.tar.gz
+    tar -xf pari-$VERSION.tar.gz
 fi
-cd pari-2.9.4
+cd pari-$VERSION
 ./Configure
 make -j4 gp
 sudo make install
